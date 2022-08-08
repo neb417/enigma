bd7b8499da2d87657ca191f0a5d30adaba5a9e1c
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-08-08 13:45:45 -0600

    writting to encrypt.rb.

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,7 +1,11 @@
 require 'pry'
+
 msg_encrypt = File.open(ARGV[0],"r")
-# binding.pry
 message_to_encrypt = msg_encrypt.read
+
+encrypted_message = File.open(ARGV[1], "w")
+
+encrypted_message.write(message_to_encrypt.upcase)
 
 msg_encrypt.close
 
