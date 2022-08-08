4d728341e887acbcadfc2b3c3c55fca96f75f0e1
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-08-08 15:29:08 -0600

    decrypt.rb reads encrypted.txt and writes to decrypted.txt

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,4 +2,7 @@ decrypt = File.new(ARGV[0], 'r')
 message_to_decrypt = decrypt.read
 decrypt.close
 
+decrypted_message = File.new(ARGV[1], 'w')
+decrypted_message.write(message_to_decrypt)
+decrypted_message.close
 puts message_to_decrypt
